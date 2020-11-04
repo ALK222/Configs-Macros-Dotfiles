8ec2755940d753129974a59c94684368f2994838
A.L.K <alejandrobarrachina.02@gmail.com>
2020-11-04 13:58:04 +0100

    update to vscode things

--- Windows-Scripts/Copy_and_Delete.ps1
+++ Windows-Scripts/Copy_and_Delete.ps1
@@ -1,6 +1,6 @@
 #Sets a backup of VsCode snippets and configuration and deletes all files in temporal folders
 
-Copy-Item -Path  "C:\Users\alex_\AppData\Roaming\Code\User\*" -Destination ".\VSCode" -Recurse -Exclude "globalStorage" "WorkspaceStorage" -Force
+Copy-Item -Path  "C:\Users\alex_\AppData\Roaming\Code\User\*" -Destination "..\VSCode" -Recurse -Exclude ("globalStorage", "WorkspaceStorage") -Force
 
 Remove-Item -Path "C:\Users\alex_\AppData\Local\Temp" -Recurse
 Remove-Item -Path "C:\Windows\Temp" -Recurse
